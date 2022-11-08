6a6bd0bb275da76dc8b1535b802a1afa262633d3
Wietze Mulder <w.mulder@vaccinatieregister.com>
2022-11-08 11:50:51 +0100

    Improved deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-IMAGE="ghcr.io/keukentrap/secure-send:main"
-CONTAINER_NAME="secure-send"
+HOST=$1
+ID=$2
+
+ssh -i $ID -p 22000 $HOST "
+IMAGE='ghcr.io/keukentrap/secure-send:main'
+CONTAINER_NAME='secure-send'
 
 mkdir -p ~/deploy/secure-send
 cd ~/deploy/secure-send
@@ -11,3 +15,4 @@ docker rm $CONTAINER_NAME
 docker pull $IMAGE
 docker run --name $CONTAINER_NAME -p 9999:9999 -d $IMAGE
 
+"
