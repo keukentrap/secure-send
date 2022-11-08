00a00cd904a13cff7516b515836028f29a024320
Wietze Mulder <w.mulder@vaccinatieregister.com>
2022-11-08 12:34:49 +0100

    cache is now persistent

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ IMAGE='ghcr.io/keukentrap/secure-send:main'
 CONTAINER_NAME='secure-send'
 
 ssh -o StrictHostKeyChecking=no -i $ID -p 22000 $HOST "
-mkdir -p ~/deploy/secure-send
+mkdir -p ~/deploy/secure-send/
 cd ~/deploy/secure-send
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker pull $IMAGE
-docker run --name $CONTAINER_NAME -p 9999:9999 -d $IMAGE
+docker run --name $CONTAINER_NAME -v /app/cache -p 9999:9999 -d $IMAGE
 "
