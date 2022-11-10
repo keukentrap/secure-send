28d2db506b2c940105f5a797aba7fb6ec8d79e23
Wietze Mulder <w.mulder@vaccinatieregister.com>
2022-11-10 11:29:46 +0100

    Deploy script now useable for different repos

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,15 @@
 set -e
 
 HOST=$1
-ID=$2
+IMAGE=$2
+CONTAINER_NAME=$3
 
-IMAGE='ghcr.io/keukentrap/secure-send:main'
-CONTAINER_NAME='secure-send'
-
-ssh -o StrictHostKeyChecking=no -i $ID -p 22000 $HOST "
-mkdir -p ~/deploy/secure-send/
-cd ~/deploy/secure-send
-
+ssh -o StrictHostKeyChecking=no -p 22000 $HOST "
 mkdir -p /tmp/cache
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker pull $IMAGE
 docker run --name $CONTAINER_NAME -v /tmp/cache:/app/cache -p 9999:9999 -d $IMAGE
+docker system purge --all --force
 "
