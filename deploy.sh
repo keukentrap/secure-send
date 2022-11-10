bef012356b06e0e934fd91d83601e2b2290305ac
Wietze Mulder <w.mulder@vaccinatieregister.com>
2022-11-10 11:38:27 +0100

    s/purge/prune/

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker pull $IMAGE
 docker run --name $CONTAINER_NAME -v /tmp/cache:/app/cache -p 9999:9999 -d $IMAGE
-docker system purge --all --force
+docker system prune --all --force
 "
